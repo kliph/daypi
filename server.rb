f0604b64a1b947d1afabd011b20466705baad41c
Cliff Rodgers <kliph@users.noreply.github.com>
2019-01-18 10:57:28 -0500

    Show today's daypi formatted date

--- server.rb
+++ server.rb
@@ -1,4 +1,23 @@
 require 'sinatra'
+
+DAYS = %w(Monday Tuesday Wednesday Thursday Friday Saturday Sunday)
+
+def month_to_daypi(ordinal_month)
+  idx = ordinal_month - 1
+  ordinal_month > 7 ? "#{DAYS[1]}#{DAYS[idx % 7]}" : "#{DAYS[0]}#{DAYS[idx % 7]}"
+end
+
+def week_of_month_to_daypi(ordinal_week_of_month)
+  DAYS[ordinal_week_of_month]
+end
+
+def date_to_daypi(date)
+  month_part = month_to_daypi(date.month)
+  week_part = week_of_month_to_daypi(date.strftime('%W').to_i)
+  day_part = date.strftime('%A')
+  month_part + week_part + day_part
+end
+
 get '/' do
-  erb :index
+  erb :index, locals: { date: date_to_daypi(Date.today) }
 end
