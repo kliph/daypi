c569502f7e88a9c53cd863a085861d5653d4fa69
Cliff Rodgers <kliph@users.noreply.github.com>
2019-01-25 13:51:09 -0500

    Consume query params as symbols

--- server.rb
+++ server.rb
@@ -14,16 +14,16 @@ get '/' do
 end
 
 get '/today' do
-  fn = FORMATS[params[:format]] || FORMATS[:default]
+  fn = FORMATS[params[:format].to_sym] || FORMATS[:default]
   erb :index, locals: fn.call(Date.today)
 end
 
 get '/month/:month/day/:day' do
-  fn = FORMATS[params[:format]] || FORMATS[:default]
+  fn = FORMATS[params[:format].to_sym] || FORMATS[:default]
   erb :index, locals: fn.call(Date.new(Date.today.year, params['month'].to_i, params['day'].to_i))
 end
 
 get '/year/:year/month/:month/day/:day' do
-  fn = FORMATS[params[:format]] || FORMATS[:default]
+  fn = FORMATS[params[:format].to_sym] || FORMATS[:default]
   erb :index, locals: fn.call(Date.new(params['year'].to_i, params['month'].to_i, params['day'].to_i))
 end
