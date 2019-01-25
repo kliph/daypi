be38dbcd9208a2ce6ac967c5b6ff01462fcd3d9e
Cliff Rodgers <kliph@users.noreply.github.com>
2019-01-25 15:17:19 -0500

    Explicitly use `self.` on each of the Module methods

--- plugins/default.rb
+++ plugins/default.rb
@@ -1,19 +1,18 @@
 module Plugins
   module Default
-    extend self
 
     DAYS = %w(Monday Tuesday Wednesday Thursday Friday Saturday Sunday)
 
-    def month_to_daypi(ordinal_month)
+    def self.month_to_daypi(ordinal_month)
       idx = ordinal_month - 1
       ordinal_month > 7 ? "#{DAYS[1]}#{DAYS[idx % 7]}" : "#{DAYS[0]}#{DAYS[idx % 7]}"
     end
 
-    def week_of_month_to_daypi(ordinal_week_of_month)
+    def self.week_of_month_to_daypi(ordinal_week_of_month)
       DAYS[ordinal_week_of_month]
     end
 
-    def date_to_daypi(date)
+    def self.date_to_daypi(date)
       month_part = month_to_daypi(date.month)
       week_part = week_of_month_to_daypi(date.week_of_month - 1)
       day_part = date.strftime('%A')
